0a13f8e61bab4d46a7f122f4bd73f7c93d6acf36
Ali Pirzada <alipirzada.khan@gmail.com>
2021-01-23 15:28:10 +0500

    send data from child component to parent component
    send onSubmit data of the form, from child component (SearchBar)
    via onFormSubmit function, to the parent Component (App)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,29 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
+  
+  // constructor(props){   
+  //   super(props);
+  //   this.onFormSubmit = this.onFormSubmit.bind(this);  // one way of fixing the value of this by binding this to function
+  // }                    
+
   state = { term: '' };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Below we call the parent method (onSearchSubmit of App component) and pass a parameter to it with this.state.term
+    // Basically we are communicating Child to Parent 
+    this.props.onSubmit(this.state.term)
+
+    // console.log(this.state.term);
+
+  }
 
   render(){
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label style={{ textAlign : 'center'}}>Image Search Engine</label>
             <input type="text" placeholder="Type your search here..." 
